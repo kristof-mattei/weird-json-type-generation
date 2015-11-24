2c5234c0d6d184b6813e46ac20b6504b84a67860
Kristof Mattei <github@kristofmattei.be>
2015-11-24 19:49:14 +0100

    Initial commit

---
+++ weird-json-type-generation/Program.fs
@@ -0,0 +1,16 @@
+﻿open FSharp.Data
+
+
+type Test = JsonProvider<"test.json">
+
+[<EntryPoint>]
+let main argv = 
+
+    let loaded = Test.Load "test.json"
+
+    let result = loaded.A234 // Where's my '1'?
+    // let result = loaded.A1234 // this one doesn't compile
+
+    printfn "%s" result
+    
+    0 
